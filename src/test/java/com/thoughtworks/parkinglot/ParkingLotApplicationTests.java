75b0bb30597a833513204474bcb68b253276fef1
Chen Lin <chen.lin@rea-group.com>
2021-03-23 16:51:05 +0800

    init repo

---
+++ src/test/java/com/thoughtworks/parkinglot/ParkingLotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.parkinglot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingLotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
